87f523ce914ca90b7edf07b8fb7f7d7fe21d6583
galAntonis <galant.dev@gmail.com>
2023-02-04 15:30:34 +0200

    Update

---
+++ app/src/main/java/com/galeos/smartalert/ChooseRoleActivity.java
@@ -0,0 +1,14 @@
+package com.galeos.smartalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseRoleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_role);
+    }
+}
