d5e864d79da03ac75e271fa50f0ed4fc5946b8ab
galAntonis <galant.dev@gmail.com>
2023-02-15 12:28:50 +0200

    Added logout option to user

--- app/src/main/java/com/galeos/smartalert/UserMainActivity.java
+++ app/src/main/java/com/galeos/smartalert/UserMainActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class UserMainActivity extends AppCompatActivity {
 
-    ImageButton sosBtn;
+    ImageButton sosBtn,logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +20,14 @@ public class UserMainActivity extends AppCompatActivity {
         sosBtn = findViewById(R.id.sosBtn);
 
         sosBtn.setOnClickListener((v)->startActivity(new Intent(UserMainActivity.this,NotifyActivity.class)));
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity((new Intent(UserMainActivity.this,ChooseRoleActivity.class)));
+                finish();
+            }
+        });
+
     }
 }
